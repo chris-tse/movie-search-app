02a993044d44ea032b6057bef9c43e125eddd5d4
Chris Tse <me@christse.dev>
2025-10-09 12:54:41 -0500

    chore: add cloudflare worker stuff

--- src/components/movie-search.tsx
+++ src/components/movie-search.tsx
@@ -253,7 +253,7 @@ function MainBody({
 	}
 
 	return (
-		<div className="mb-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+		<div className="mb-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
 			{movies?.map((movie) => (
 				<MovieCard key={movie.id} movie={movie} onClick={onCardClick} onHover={onCardHover} />
 			))}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		cloudflare(),
 		tailwindcss(),
 		react({
 			babel: {
